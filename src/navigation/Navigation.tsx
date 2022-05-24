d37b91cc324777dd7d4c9a2184fdf3b195921cd4
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2022-05-24 19:10:12 +0200

    feat(navitaion): add mobile styles, and nav buttons

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,21 +1,28 @@
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
+import MenuIcon from '@mui/icons-material/Menu';
 import {routesPaths} from "../routes";
 
 import './styles.css';
 
 export const Navigation = () => {
+    const [navigationVisible, setNavigationVisible] = useState<boolean>(true)
+    const handleShowMobileNav = () => {
+        setNavigationVisible(!navigationVisible)
+    }
+
     return (
         <div className='Navigation'>
             <div>
                 logo
+                <MenuIcon onClick={handleShowMobileNav} className='Navigation-mobile'/>
             </div>
-            <div>
+            {navigationVisible && <div className='Navigation-links'>
                 <Link className='App-link' to={'/'}>Home</Link>
                 <Link className='App-link' to={`/${routesPaths.about}`}>About</Link>
                 <Link className='App-link' to={'/form'}>Form</Link>
                 <Link className='App-link' to={'/policy'}>Policy</Link>
-            </div>
+            </div>}
         </div>
     )
 }

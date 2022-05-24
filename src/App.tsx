277c49064b292cfad22c8906d38389c3a4f451ef
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2022-05-24 16:57:36 +0200

    feat(packages): navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import {Routes, Route, Link} from "react-router-dom";
+
 import './App.css';
 import {InputForm} from "./InputForm";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <InputForm defaultValue={'hello form'}/>
-            </header>
+            <div className='App-header'>
+                <div>
+                    logo
+                </div>
+                <div>
+                    <Link className='App-link' to={'/'}>Home</Link>
+                    <Link className='App-link' to={'/about'}>About</Link>
+                    <Link className='App-link' to={'/form'}>Form</Link>
+                </div>
+            </div>
+            <Routes>
+                <Route path='/' element={<InputForm defaultValue={'hello form'}/>}/>
+                <Route path='*' element={<div>404</div>}/>
+                <Route path='/about' element={<div>About</div>}/>
+                <Route path='/form' element={<div>Form</div>}/>
+            </Routes>
         </div>
     );
 }
